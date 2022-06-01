7e4d861fa215a1a899c36d8116fee37566505f4b
Jordan Neal <jordaneal@gmail.com>
2022-06-01 12:23:37 -0700

    removed icon on SS

--- TestHarness/SplashScreen.Designer.cs
+++ TestHarness/SplashScreen.Designer.cs
@@ -61,7 +61,6 @@
             this.Controls.Add(this.progressBar);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SplashScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashScreen";
